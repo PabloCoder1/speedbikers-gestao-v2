23227bfdc73790130424cd0187465d3e43b3c3ef
PabloCoder1 <pablolimacoder@gmail.com>
2026-08-07 15:04:44 -0300

    chore: configure mercado livre applications

---
+++ src/integrations/mercado-livre/constants.ts
@@ -0,0 +1,10 @@
+export const MERCADO_LIVRE_URLS = {
+  authorization:
+    "https://auth.mercadolivre.com.br/authorization",
+
+  token:
+    "https://api.mercadolibre.com/oauth/token",
+
+  api:
+    "https://api.mercadolibre.com",
+} as const;
